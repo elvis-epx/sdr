19ea0d76b68780fa2aedd93f106f70ae893e9a12
Elvis Pfützenreuter <epxx@epxx.co>
2019-01-16 11:08:38 -0200

    Small refactoring

--- filters2.py
+++ filters2.py
@@ -3,22 +3,44 @@
 import numpy, math, sys
 from numpy import fft
 
+def impulse(mask):
+	# Negative side, a mirror of positive side
+	negatives = mask[1:-1]
+	negatives.reverse()
+	mask = mask + negatives
+	fft_length = len(mask)
+
+	# Convert FFT filter mask to FIR coefficients
+	impulse_response = fft.ifft(mask).real.tolist()
+
+	# swap left and right sides
+	left = impulse_response[:fft_length // 2]
+	right = impulse_response[fft_length // 2:]
+	impulse_response = right + left
+
+	return impulse_response
+
+
 def fir_coefs(sample_rate, pass_lo, cutoff_lo, cutoff_hi, pass_hi):
 	assert pass_lo is None or cutoff_lo > pass_lo
 	assert pass_hi is None or pass_hi > cutoff_hi
-	nyquist_rate = sample_rate / 2.0
-	fft_length = 60
+	max_freq = sample_rate / 2.0
+
 	if cutoff_lo is not None:
 		trans = cutoff_lo - pass_lo
 	else:
 		trans = 1
+
 	if cutoff_hi is not None:
 		trans = max(trans, pass_hi - cutoff_hi)
+
 	bt = max(trans, 1) / sample_rate
-	fft_length /= 22 * bt
-	fft_length = int(fft_length / 2) * 2
-	print("Taps: %d" % fft_length, file=sys.stderr)
-	f2s = nyquist_rate / (fft_length / 2.0)
+	tap_count = 60 # dB attenuation
+	tap_count /= 22 * bt
+	tap_count = int(tap_count / 2) * 2
+	print("Taps: %d" % tap_count, file=sys.stderr)
+
+	f2s = max_freq / (tap_count / 2.0)
 
 	if cutoff_lo is not None:
 		cutoff_lo /= f2s
@@ -30,10 +52,9 @@ def fir_coefs(sample_rate, pass_lo, cutoff_lo, cutoff_hi, pass_hi):
 		cutoff_hi /= f2s
 		pass_hi /= f2s
 		step_hi = 1.0 / (pass_hi - cutoff_hi)
-		print("hi %f %f %f" % (pass_hi, cutoff_hi, step_hi), file=sys.stderr)
 
 	# create FFT filter mask
-	l = fft_length // 2
+	l = tap_count // 2
 	mask = [ 0 for f in range(0, l+1) ]
 	
 	if pass_lo is not None:
@@ -59,22 +80,7 @@ def fir_coefs(sample_rate, pass_lo, cutoff_lo, cutoff_hi, pass_hi):
 				tap = 0.0
 			mask[f] *= tap
 
-	print(mask, file=sys.stderr)
-
-	# Negative side, a mirror of positive side
-	negatives = mask[1:-1]
-	negatives.reverse()
-	mask = mask + negatives
-
-	# Convert FFT filter mask to FIR coefficients
-	impulse_response = fft.ifft(mask).real.tolist()
-
-	# swap left and right sides
-	left = impulse_response[:fft_length // 2]
-	right = impulse_response[fft_length // 2:]
-	impulse_response = right + left
-
-	return impulse_response
+	return impulse(mask)
 
 class filter:
 	def __init__(self, sample_rate, cutoff):
