b8b7169bd15c7c059bd6b655f78cf5ce888a1886
Elvis Pfützenreuter <epxx@epxx.co>
2019-02-24 00:51:44 -0300

    First working example (for wideband FM)

--- talkabout.py
+++ talkabout.py
@@ -2,7 +2,7 @@
 
 import struct, numpy, sys, math, wave, filters
 
-INPUT_RATE = 256000
+INPUT_RATE = 960000
 
 '''
 MAX_DEVIATION = 10000 # Hz
@@ -10,21 +10,24 @@ CENTER=462637500
 freqs = [462562500, 462587500, 462612500, 462637500, 462662500, 462687500, 462712500,
 	 462550000, 462575000, 462600000, 462625000, 462650000, 462675000, 462675000,
 	 462725000]
-AUDIO_BANDWIDTH = 3400
 IF_BANDWIDTH = 20000
+IF_RATE = 40000
+AUDIO_BANDWIDTH = 3400
 AUDIO_RATE = 8000
 '''
 
 MAX_DEVIATION = 200000 # Hz
-CENTER = 93100000
+CENTER = 92900000
 freqs = [93100000]
-IF_BANDWIDTH = 250000
+IF_BANDWIDTH = 200000
+IF_RATE = 240000
 AUDIO_BANDWIDTH = 15000
-AUDIO_RATE = 32000
+AUDIO_RATE = 48000
 
-assert (INPUT_RATE // AUDIO_RATE) == (INPUT_RATE / AUDIO_RATE)
+assert (INPUT_RATE // IF_RATE) == (INPUT_RATE / IF_RATE)
+assert (IF_RATE // AUDIO_RATE) == (IF_RATE / AUDIO_RATE)
 
-DEVIATION_X_SIGNAL = 0.99 / (math.pi * MAX_DEVIATION / (INPUT_RATE / 2))
+DEVIATION_X_SIGNAL = 0.99 / (math.pi * MAX_DEVIATION / (IF_RATE / 2))
 
 tau = 2 * math.pi
 
@@ -35,32 +38,40 @@ class Demodulator:
 		self.wav.setnchannels(1)
 		self.wav.setsampwidth(1)
 		self.wav.setframerate(AUDIO_RATE)
-		# since samples are complex, filter goes from -freq to +freq
-		self.if_filter = filters.low_pass(INPUT_RATE, IF_BANDWIDTH / 2, 48)
-		# real samples, filter goes from 0 to +freq
-		self.audio_filter = filters.band_pass(INPUT_RATE, 30, AUDIO_BANDWIDTH, 24)
-		self.decimator = filters.decimator(INPUT_RATE // AUDIO_RATE)
-		self.if_freq = freq - CENTER
+
+		# IF control
+		self.if_freq = CENTER - freq
 		self.if_angle = 0
+		self.if_adv = tau * self.if_freq / INPUT_RATE
 		self.last_if_sample = []
+
+		# IF filtering
+		# complex samples, filter goes from -freq to +freq
+		self.if_filter = filters.low_pass(INPUT_RATE, IF_BANDWIDTH / 2, 48)
+		self.if_decimator = filters.decimator(INPUT_RATE // IF_RATE)
+
+		# Audio filter
+		self.audio_filter = filters.band_pass(IF_RATE, 30, AUDIO_BANDWIDTH, 24)
+		self.audio_decimator = filters.decimator(IF_RATE // AUDIO_RATE)
+
 
 	def ingest(self, iqsamples):
 		# Center frequency of samples on desired frequency
 		ifsamples = []
 		for i in range(0, len(iqsamples)):
-			self.if_angle = (self.if_angle + tau * self.if_freq / INPUT_RATE) % tau
+			self.if_angle = (self.if_angle + self.if_adv) % tau
 			if_carrier = math.cos(self.if_angle)
-			if_carrier = 1
 			ifsamples.append(if_carrier * iqsamples[i])
 
 		# Get last sample from last batch
 		ifsamples = self.last_if_sample + ifsamples
 
-		# Filter IF to bandwidth
-		# ifsamples = self.if_filter.feed(ifsamples)
-
 		# Save last sample to next batch
 		self.last_if_sample = ifsamples[-1:]
+
+		# Filter IF to radio bandwidth and decimate
+		ifsamples = self.if_filter.feed(ifsamples)
+		ifsamples = self.if_decimator.feed(ifsamples)
 
 		# Finds angles (phase) of I/Q pairs
 		angles = numpy.angle(ifsamples)
@@ -77,9 +88,9 @@ class Demodulator:
 		output_raw = numpy.multiply(rotations, DEVIATION_X_SIGNAL)
 		output_raw = numpy.clip(output_raw, -0.999, +0.999)
 
-		# Filter audio to bandwidth and decimate
+		# Filter to audio bandwidth and decimate
 		output_raw = self.audio_filter.feed(output_raw)
-		output_raw = self.decimator.feed(output_raw)
+		output_raw = self.audio_decimator.feed(output_raw)
 
 		# Scale to unsigned 8-bit int with offset (8-bit WAV)
 		output_raw = numpy.multiply(output_raw, 127) + 127
@@ -94,8 +105,7 @@ for freq in freqs:
 
 # Prefilter to get 200kHz out of 1M band
 # Complex samples, bandwidth goes from -freq to +freq
-# prefilter = filters.low_pass(INPUT_RATE, 400000, 24)
-# prefilter = filters.low_pass(INPUT_RATE, 100000, 24)
+prefilter = filters.low_pass(INPUT_RATE, 400000, 24)
 
 remaining_data = b''
 	
@@ -118,5 +128,4 @@ while True:
 
 	# Forward I/Q samples to all channels
 	for k, d in demodulators.items():
-		# d.ingest(prefilter.feed(iqdata))
-		d.ingest(iqdata)
+		d.ingest(prefilter.feed(iqdata))
