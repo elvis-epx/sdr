5c3c91e8d1c41cc7534980eb28b8653f82896e1d
Elvis Pfützenreuter <epxx@epxx.co>
2019-02-24 23:33:29 -0300

    Use cos table to speed up IF demodulation

--- vhf
+++ vhf
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 
 # rtl_sdr -f 92.9M -s 960k -n 1920000 ta.iq
-rtl_sdr -f 152500000 -g 30 -s 960k - | ./vhf.py
+rtl_sdr -f 152500000 -g 30 -s 1000k - | ./vhf.py
--- vhf.py
+++ vhf.py
@@ -2,21 +2,28 @@
 
 import struct, numpy, sys, math, wave, filters
 
-INPUT_RATE = 960000
+INPUT_RATE = 1000000
 
 MAX_DEVIATION = 10000 # Hz
 CENTER = 152500000 
 freqs = [152390000]
+STEP = 2500
 IF_BANDWIDTH = 20000
-IF_RATE = 20000
+IF_RATE = 25000
 AUDIO_BANDWIDTH = 4000
-AUDIO_RATE = 10000
+AUDIO_RATE = 12500
 
 # -45..-48 dbFS is the minimum, 6db = 1 bit of audio
 THRESHOLD = -39
 
 assert (INPUT_RATE // IF_RATE) == (INPUT_RATE / IF_RATE)
 assert (IF_RATE // AUDIO_RATE) == (IF_RATE / AUDIO_RATE)
+
+# Makes sure IF demodulation carrier will be a multiple of STEP Hz
+assert (INPUT_RATE / STEP == INPUT_RATE // STEP)
+for f in freqs:
+	if_freq = abs(CENTER - f)
+	assert(if_freq / STEP == if_freq // STEP)
 
 DEVIATION_X_SIGNAL = 0.99 / (math.pi * MAX_DEVIATION / (IF_RATE / 2))
 
@@ -32,12 +39,17 @@ class Demodulator:
 		self.recording = False
 
 		# Energy estimation
-		self.energy = 0
+		self.energy = -48
 
-		# IF control
+		# IF
 		self.if_freq = CENTER - freq
-		self.if_angle = 0
-		self.if_adv = tau * self.if_freq / INPUT_RATE
+		# works because both if_freq and INPUT_RATE are multiples of STEP
+		self.carrier_table = [ math.cos(t * tau * (self.if_freq / INPUT_RATE))
+				for t in range(0, INPUT_RATE // 4) ]
+		self.carrier_table = numpy.array(self.carrier_table)
+		# actually, this is the biggest period (carrier of 2500Hz)
+		self.if_period = INPUT_RATE // STEP
+		self.if_phase = 0
 		self.last_if_sample = []
 
 		# IF filtering
@@ -46,17 +58,19 @@ class Demodulator:
 		self.if_decimator = filters.decimator(INPUT_RATE // IF_RATE)
 
 		# Audio filter
-		self.audio_filter = filters.band_pass(IF_RATE, 30, AUDIO_BANDWIDTH, 36)
+		self.audio_filter = filters.band_pass(IF_RATE, 30, AUDIO_BANDWIDTH, 24)
 		self.audio_decimator = filters.decimator(IF_RATE // AUDIO_RATE)
 
 
 	def ingest(self, iqsamples):
 		# Center frequency of samples on desired frequency
-		ifsamples = []
-		for i in range(0, len(iqsamples)):
-			self.if_angle = (self.if_angle + self.if_adv) % tau
-			if_carrier = math.cos(self.if_angle)
-			ifsamples.append(if_carrier * iqsamples[i])
+
+		# Get a cosine table in correct phase
+		carrier = self.carrier_table[self.if_phase:self.if_phase + len(iqsamples)]
+		# Advance phase
+		self.if_phase = (self.if_phase + len(iqsamples)) % self.if_period
+		# Demodulate
+		ifsamples = iqsamples * carrier
 
 		# Filter IF to radio bandwidth and decimate
 		ifsamples = self.if_filter.feed(ifsamples)
@@ -75,17 +89,17 @@ class Demodulator:
 		energy = numpy.sum(numpy.absolute(ifsamples)) \
 			* math.sqrt(INPUT_RATE / IF_RATE) \
 			/ len(ifsamples)
-		self.energy = 0.5 * energy + 0.5 * self.energy
-		db = 20 * math.log10(self.energy)
+		db = 20 * math.log10(energy)
+		self.energy = 0.5 * db + 0.5 * self.energy
 
-		print("%f: signal %f dbFS" % (self.freq, db))
+		print("%f: signal %f dbFS" % (self.freq, self.energy))
 		if not self.recording:
-			if db > THRESHOLD:
-				print("%f: signal %f dbFS, starting to record" % (self.freq, db))
+			if self.energy > THRESHOLD:
+				print("%f: signal %f dbFS, starting to record" % (self.freq, self.energy))
 				self.recording = True
 		else:
-			if db < THRESHOLD:
-				print("%f: signal %f dbFS, stopping to record" % (self.freq, db))
+			if self.energy < THRESHOLD:
+				print("%f: signal %f dbFS, stopping to record" % (self.freq, self.energy))
 				self.recording = False
 
 		if not self.recording:
@@ -140,6 +154,7 @@ while True:
 	# Convert to complex numbers
 	iqdata = [ ((data[n * 2 + 0] - 127.5) + (0+1j) * (data[n * 2 + 1] - 127.5)) / 128.0
 		for n in range(0, len(data) // 2) ]
+	iqdata = numpy.array(iqdata)
 
 	# Forward I/Q samples to all channels
 	for k, d in demodulators.items():
