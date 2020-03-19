72649bf8d24357c0673fd100ef0facef0a41cc03
Elvis Pfützenreuter <epxx@epxx.co>
2020-03-18 22:28:30 -0300

    Improvements

--- sortin/quicksort_easy.py
+++ sortin/quicksort_easy.py
@@ -3,61 +3,52 @@
 import random
 import math
 
-COUNT=1000
-a = [ int(random.random() * 1000) for i in range(0, COUNT) ]
+COUNT=15
+a = [ int(random.random() * 100) for i in range(0, COUNT) ]
 print(a)
 
 def quicksort(a, start, end, level):
-	print(" " * level + "start=%d end=%d %s" % (start, end, str(a[start:end])))
 	length = end - start
+	if length <= 1:
+		return
 
-	# Use average of 3 as pivot value
 	mid = start + length // 2
 	pivot_value = (a[start] + a[mid] + a[end-1]) // 3
 
-	# Count how many elements are <= pivot, also min and max
-	pivot = start
-	lowest = start
-	lowest_value = a[start]
-	highest = start
-	highest_value = a[start]
+	# Find pivot point by counting how many elements are <= pivot_value
+	# Also, determine minimum and maximum values to handle problem cases
+	pivot, lowest, highest = start, start, start
 	for i in range(start, end):
 		if a[i] <= pivot_value:
 			pivot += 1
-		if a[i] < lowest_value:
-			lowest_value = a[i]
+		if a[i] < a[lowest]:
 			lowest = i
-		if a[i] > highest_value:
-			highest_value = a[i]
+		if a[i] > a[highest]:
 			highest = i
 
-	print(" " * level + "pivot=%d, pivot_value=%d" % (pivot, pivot_value))
-
 	if lowest == highest:
-		print(" " * level + "all equal")
+		# Pathologcal case: all values are equal; do nothing
 		return
-
-	if pivot == start or pivot == end:
+	elif pivot == start or pivot == end:
+		# Pathological case: badly chosen pivot value
+		# Fix by limiting left side to 1 element
 		pivot = start + 1
-		pivot_value = lowest_value
-		print(" " * level + "pathological")
+		pivot_value = a[lowest]
 
-	# Move elements > pivot to the right side
+	# Move elements bigger than pivot_value to the right side
 	j = pivot
 	for i in range(start, pivot):
-		if a[i] > pivot_value:
-			# Find element <= pivot at right side
-			while a[j] > pivot_value:
-				j += 1
-			# Exchange
-			a[i], a[j] = a[j], a[i]
-	print(" " * level + "after move: %s %s" % (str(a[start:pivot]), str(a[pivot:end])))
+		if a[i] <= pivot_value:
+			# This element should stay at left side
+			continue
+		# If there is a wrong element at left side,
+		# there must be a wrong element at right side.
+		while a[j] > pivot_value:
+			j += 1
+		a[i], a[j] = a[j], a[i]
 
-	# Recursively sort each side with at least 2 elements
-	if (start + 1) < pivot:
-		quicksort(a, start, pivot, level + 1)
-	if (pivot + 1) < end:
-		quicksort(a, pivot, end, level + 1)
+	quicksort(a, start, pivot, level + 1)
+	quicksort(a, pivot, end, level + 1)
 
 quicksort(a, 0, len(a), 0)
 print(a)
--- sortin/quicksort_opt.py
+++ sortin/quicksort_opt.py
@@ -3,47 +3,36 @@
 import random
 import math
 
-COUNT=1000
-a = [ int(random.random() * 1000) for i in range(0, COUNT) ]
+COUNT=12
+a = [ int(random.random() * 100) for i in range(0, COUNT) ]
 print(a)
 
 def quicksort(a, start, end, level):
 	length = end - start
+	if length <= 1:
+		return
+	print(" " * level, a[start:end])
 
 	# Use average of 3 as pivot value
 	mid = start + length // 2
 	pivot_value = (a[start] + a[mid] + a[end-1]) // 3
-	print(" " * level + "start=%d end=%d pivot=%d" % (start, end, pivot_value))
 
 	# Scan from both ends until pointers meet
-	i = start
-	j = end - 1
-
-	# To work correctly with len=2, use i <= j instead of i < j
+	i, j = start, end - 1
+	# Logic is carefully crafted to handle lists with size=2
+	# and uniform lists
 	while i <= j:
-		# print(" " * level + "i=%d(%d) j=%d(%d) start=%d end=%d %s" % (i, a[i], j, a[j], start, end, a[start:end]))
-		# Find a big element at left
 		while a[i] < pivot_value:
 			i += 1
-		# Find a small element at right
 		while a[j] > pivot_value:
 			j -= 1
-		# print(" " * level + "   i=%d(%d) j=%d(%d) " % (i, a[i], j, a[j]))
-		# and swap them
-		# To work correctly with len=2, use i <= j instead of i < j
 		if i <= j:
-			# print(" " * level + "Swapping %d(%d) and %d(%d)" % (i, a[i], j, a[j]))
 			a[i], a[j] = a[j], a[i]
 			i += 1
 			j -= 1
 
-	print(" " * level + "%s %s" % (a[start:i], a[i:end]))
-
-	# Recursively sort each side with at least 2 elements
-	if (start + 1) < i:
-		quicksort(a, start, i, level + 1)
-	if (i + 1) < end:
-		quicksort(a, i, end, level + 1)
+	quicksort(a, start, i, level + 1)
+	quicksort(a, i, end, level + 1)
 
 quicksort(a, 0, len(a), 0)
 print(a)
