67aac4a80b60400719529611e387d63b8143e73b
Elvis Pfutzenreuter <epxx@epxx.co>
2024-12-22 00:22:34 -0300

    Initial work on OOK 433MHz decoder

---
+++ ook_433.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+# Estimate signal strengh
+
+import struct, numpy, sys, math, filters
+
+gain = float(sys.argv[1])
+
+INPUT_RATE = 960000
+FPS = 10
+BANDWIDTH = 240000
+IF_RATE = BANDWIDTH
+
+SAMPLE_US = 1000000 / IF_RATE
+
+remaining_data = b''
+
+if_filter = filters.low_pass(INPUT_RATE, BANDWIDTH / 2, 48)
+if_decimator = filters.decimator(INPUT_RATE // IF_RATE)
+
+bgnoise = 0.66 / 128 # 2/3 of one bit
+ook_threshold_db = 10 # dB above background noise
+ook_threshold = bgnoise * (10 ** (ook_threshold_db / 10))
+
+state = 0
+length = 0
+
+while True:
+    # Ingest up to 1/FPS worth of data
+    data = sys.stdin.buffer.read(INPUT_RATE * 2 // FPS)
+    if not data:
+        break
+    data = remaining_data + data
+    remaining_data = b''
+
+    if len(data) < 4:
+        remaining_data = data
+        continue
+
+    # Parse RTL-SDR I/Q format
+    iqdata = numpy.frombuffer(data, dtype=numpy.uint8)
+    iqdata = iqdata - 127.4
+    iqdata = iqdata / 128.0
+    iqdata = iqdata.view(complex)
+
+    # Filter and downsample to bandwidth of interest
+    iqdata = if_filter.feed(iqdata)
+    iqdata = if_decimator.feed(iqdata)
+
+    # We are only interested in absolute amplitude
+    iqdata = numpy.absolute(iqdata)
+
+    # Calculate background noise & moving average
+    totenergy = numpy.sum(iqdata)
+    counter = len(iqdata)
+    avg = totenergy / counter
+    bgnoise = bgnoise * 0.999 + avg * 0.001
+    print("bgnoise %f %f" % (bgnoise, avg))
+
+    for sample in iqdata:
+        if state == 0:
+            if sample > ook_threshold:
+                print("-%d", int(length * SAMPLE_US))
+                length = 0
+                state = 1
+        else:
+            if sample < ook_threshold:
+                print("+%d", int(length * SAMPLE_US))
+                length = 0
+                state = 0
+        length += 1
