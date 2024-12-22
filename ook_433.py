0b2fc47aad99ad8b936a22281c8fbe6507f3c123
Elvis Pfutzenreuter <epxx@epxx.co>
2024-12-22 10:31:45 -0300

    Fix threshold

--- ook_433.py
+++ ook_433.py
@@ -19,8 +19,10 @@ if_filter = filters.low_pass(INPUT_RATE, BANDWIDTH / 2, 48)
 if_decimator = filters.decimator(INPUT_RATE // IF_RATE)
 
 bgnoise = 0.66 / 128 # 2/3 of one bit
-ook_threshold_db = 10 # dB above background noise
-ook_threshold = bgnoise * (10 ** (ook_threshold_db / 10))
+ook_threshold_db_up = 10 # dB above background noise
+ook_threshold_db_down = 8
+ook_threshold_mul_up = 10 ** (ook_threshold_db_up / 10)
+ook_threshold_mul_down = 10 ** (ook_threshold_db_down / 10)
 
 state = 0
 length = 0
@@ -55,16 +57,18 @@ while True:
     counter = len(iqdata)
     avg = totenergy / counter
     bgnoise = bgnoise * 0.999 + avg * 0.001
+    ook_threshold_up = bgnoise * ook_threshold_mul_up
+    ook_threshold_down = bgnoise * ook_threshold_mul_down
     print("bgnoise %f %f" % (bgnoise, avg))
 
     for sample in iqdata:
         if state == 0:
-            if sample > ook_threshold:
+            if sample > ook_threshold_up:
                 print("-%d", int(length * SAMPLE_US))
                 length = 0
                 state = 1
         else:
-            if sample < ook_threshold:
+            if sample < ook_threshold_down:
                 print("+%d", int(length * SAMPLE_US))
                 length = 0
                 state = 0
