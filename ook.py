7b1c699e50be7ac9fc04a42f3cef6edffc7ccd65
Elvis Pfutzenreuter <epxx@epxx.co>
2024-12-22 22:45:57 -0300

    First functional OOK receiver

---
+++ ook.py
@@ -0,0 +1,104 @@
+class OOKParser:
+    # Generic ASK/OOK parser for 433MHz keyfobs
+    # Assumption: every bit is composed of two level transitions
+
+    name = ""
+    bit_len_tolerance = 0.15
+
+    # bitseq LH = bit is formed by a transition to low, then a transition to high
+    #             and first transition is a delimiter
+    #             Chirp examples: 001=1, 011=0
+    # bitseq HL = bit is formed by a transition to high, then a transition to low
+    #             and last transition is a delimiter
+    #             Chirp examples: 1110=1, 1000=0
+    bitseq = "LH"
+
+    # bit1 LS = bit 1 is formed by a long-timed level followed by a short-timed level
+    #           (and bit 0 is the opposite)
+    #           Chirp examples: 1110=1 and 1000=0, or 001=1 and 011=0
+    # bit1 SL = bit 1 is formed by a short-timed level followed by a long-timed level
+    #           Chirp examples: 1110=0 and 1000=1, or 001=0 and 011=1
+    bit1 = "LS" 
+
+    def __init__(self, sequence):
+        self.sequence = sequence
+        self.code = 0
+
+    def res(self):
+        return "%s:%d" % (self.name, self.code)
+
+    # Calculate average timing of each bit
+    def bit_timing(self, bitcount, lh):
+        tot = totsq = 0
+        for i in range(0, bitcount):
+            bittime = abs(self.sequence[i*2+lh]) + abs(self.sequence[i*2+1+lh])
+            tot += bittime
+            totsq += bittime * bittime
+        mean = tot / bitcount
+        return mean, (totsq / bitcount - mean * mean) ** 0.5
+
+    # Find if timing of a single bit is off
+    def anomalous_bit_timing(self, bitcount, lh, std, dev):
+        for i in range(0, bitcount):
+            bit_time = abs(self.sequence[i*2+lh]) + abs(self.sequence[i*2+1+lh])
+            if bit_time < (std - dev) or bit_time > (std + dev):
+                return (i, bit_time)
+        return None
+
+    # Parsing routine
+    def parse(self):
+        if len(self.sequence) != self.exp_sequence_len:
+            return False
+
+        bitcount = len(self.sequence) // 2
+        lh = (self.bitseq == "LH") and 1 or 0
+        ls = (self.bitseq == "LS") and 1 or 0
+
+        bit_time, bit_time_dev = self.bit_timing(bitcount, lh)
+        print(self.name, "> bit timing %dus stddev %dus" % (bit_time, bit_time_dev))
+
+        anom = self.anomalous_bit_timing(bitcount, lh, bit_time, bit_time * self.bit_len_tolerance)
+        if anom:
+            print(self.name, "> bit timing anomaly %d timing %d" % anom)
+            return False
+
+        # Parse sane sequence
+        self.code = 0
+        for i in range(0, bitcount):
+            lsbit = (abs(self.sequence[i*2+lh]) > abs(self.sequence[i*2+1+lh])) and 1 or 0
+            self.code = (self.code << 1) | (lsbit ^ ls)
+
+        return True
+
+class HT6P20(OOKParser):
+    name = "HT6P20"
+    exp_sequence_len = 57
+    bitseq = "LH" # low then high (011, 001)
+    bit1 = "LS" # long then short (001)
+
+    def parse(self):
+        if not super().parse():
+            return False
+        if (self.code & 0xf) != 0b0101:
+            print(self.name, "> suffix 0101 not found")
+            return False
+        return True
+
+class EV1527(OOKParser):
+    name = "EV1527"
+    exp_sequence_len = 49
+    bitseq = "HL" # high then low (1000 and 1110)
+    bit1 = "LS" # long then short (1110)
+
+parsers = [EV1527, HT6P20]
+
+def parse(data):
+    print("--- received data, length %d" % len(data))
+    for parser_class in parsers:
+        parser = parser_class(data)
+        if parser.parse():
+            res = parser.res()
+            print(res)
+            break
+    else:
+        print("... failed to parse")
--- ook_433.py
+++ ook_433.py
@@ -2,16 +2,27 @@
 
 # Estimate signal strengh
 
-import struct, numpy, sys, math, filters
+import struct, numpy, sys, math, filters, ook
 
 gain = float(sys.argv[1])
 
 INPUT_RATE = 960000
-FPS = 10
+FPS = 5
 BANDWIDTH = 240000
 IF_RATE = BANDWIDTH
 
+# Length in µs per sample
 SAMPLE_US = 1000000 / IF_RATE
+# Minimum transition length to be considered part of a signal
+TRANS_MIN_US = 150
+# Maximum
+TRANS_MAX_US = 1500
+# Minimum transition length to be considered a signal preamble
+PREAMBLE_MIN_US = 5000
+# Maximum
+PREAMBLE_MAX_US = 20000
+# Transitions less than this are considered glitches
+GLITCH_US = min(TRANS_MIN_US, PREAMBLE_MIN_US) / 4
 
 remaining_data = b''
 
@@ -26,6 +37,10 @@ ook_threshold_mul_down = 10 ** (ook_threshold_db_down / 10)
 
 state = 0
 length = 0
+samples = []
+
+def sgn(x):
+    return (x >= 0) and +1 or -1
 
 while True:
     # Ingest up to 1/FPS worth of data
@@ -56,21 +71,70 @@ while True:
     totenergy = numpy.sum(iqdata)
     counter = len(iqdata)
     avg = totenergy / counter
-    weight = 0.02 / FPS
+    weight = 0.01 / FPS
     bgnoise = bgnoise * (1.0 - weight) + avg * weight
     ook_threshold_up = bgnoise * ook_threshold_mul_up
     ook_threshold_down = bgnoise * ook_threshold_mul_down
-    print("bgnoise %f %f" % (bgnoise, avg))
+    # print("bgnoise %f %f" % (bgnoise, avg))
 
+    # Detect transitions
     for sample in iqdata:
         if state == 0:
             if sample > ook_threshold_up:
-                print("-%d" % int(length * SAMPLE_US))
+                # transition 0 -> 1
+                samples.append(-int(length * SAMPLE_US))
                 length = 0
                 state = 1
-        else:
+        elif state == 1:
             if sample < ook_threshold_down:
-                print("+%d" % int(length * SAMPLE_US))
+                # transition 1 -> 0
+                samples.append(+int(length * SAMPLE_US))
+                length = 0
+                state = 2
+        elif state == 2:
+            if sample > ook_threshold_up:
+                # transition 0 -> 1 (alternate path)
+                samples.append(-int(length * SAMPLE_US))
+                length = 0
+                state = 1
+            elif length * SAMPLE_US > PREAMBLE_MAX_US:
+                # early annotation of long 0 that serves as stop mark
+                samples.append(-int(length * SAMPLE_US))
                 length = 0
                 state = 0
         length += 1
+
+    # Filter out short transitions that must be glitches
+    i = 0
+    while i < len(samples):
+        if abs(samples[i]) <= GLITCH_US:
+            if i > 0:
+                # Add time to previous sample
+                samples[i - 1] += samples[i] * sgn(samples[i - 1])
+            del samples[i]
+            continue
+        elif i > 0 and sgn(samples[i]) == sgn(samples[i - 1]):
+            # Two samples with same sign, possibly because of glitch removal, consolidate
+            samples[i - 1] += samples[i]
+            del samples[i]
+            continue
+        i += 1
+
+    # Cut signals and send to processing
+    while True:
+        # Detect preambles
+        preambles = [i for i, val in enumerate(samples) if val < -PREAMBLE_MIN_US]
+        if not preambles:
+            samples = []
+            break
+
+        if len(preambles) >= 2:
+            # Signal = samples between two preambles
+            start = preambles[0] + 1
+            stop = preambles[1]
+            ook.parse(samples[start:stop])
+            samples = samples[stop:]
+        elif len(preambles) == 1:
+            stop = preambles[0]
+            samples = samples[stop:]
+            break
